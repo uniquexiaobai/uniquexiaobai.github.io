30c59dc17166e65a4f081ff923a982d11fcb4fc8
uniquexiaobai <unique_xiaobai@qq.com>
2018-05-24 20:05:50 +0800

    network first

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 
 workbox.routing.registerRoute(
 	new RegExp('/(index.html)?'),
-	workbox.strategies.staleWhileRevalidate()
+	workbox.strategies.networkFirst()
 );
 
 workbox.routing.registerRoute(
 	new RegExp('https?://cn.bing.com'),
-	workbox.strategies.staleWhileRevalidate()
+	workbox.strategies.networkFirst()
 );
 
 workbox.routing.registerRoute(
